fef1255cca01532dd1251d2c369afe765f446233
eminorhan <aeminorhan@gmail.com>
2022-10-04 12:59:45 -0400

    make clm script slurmy

--- run_clm_no_trainer.sh
+++ run_clm_no_trainer.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=200GB
+#SBATCH --time=00:10:00
+#SBATCH --job-name=run_clm
+#SBATCH --output=run_clm_%A_%a.out
+#SBATCH --array=0
+
+module purge
+module load cuda/11.3.1
+
 python run_clm_no_trainer.py \
     --train_file data.json \
     --per_device_train_batch_size 1 \
