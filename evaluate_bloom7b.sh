2e98017afa041c6790f501c8f534ea49f73410e9
eminorhan <aeminorhan@gmail.com>
2022-11-02 22:54:52 -0400

    bloom-7b train & evaluate

---
+++ evaluate_bloom7b.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=240GB
+#SBATCH --time=4:00:00
+#SBATCH --job-name=evaluate_bloom7b
+#SBATCH --output=evaluate_bloom7b_%A_%a.out
+#SBATCH --array=0
+
+module purge
+module load cuda/11.6.2
+
+# which experiment
+EXPT="expt1"
+
+# grid
+EXS=("seen_data_0" "seen_data_1" "seen_data_2" "seen_data_3")
+LRS=(0.0001 0.00003 0.00001 0.000003)
+BSS=(1 2)
+
+# bloom-7b
+for EX in "${EXS[@]}"
+do
+    for LR in "${LRS[@]}"
+    do
+        for BS in "${BSS[@]}"
+        do
+            SP="bloom_7b_${EX}_${LR}_${BS}"
+            python -u /scratch/eo41/lm-recognition-memory/evaluate.py \
+                --model_name_or_path "models/${SP}" \
+                --seen_file "data/recognition-memory-experimental-data/${EXPT}/${EX}.json" \
+                --unseen_file "data/recognition-memory-experimental-data/${EXPT}/un${EX}.json" \
+                --per_device_eval_batch_size 1 \
+                --output_dir "evals/${EXPT}-bloom7b" \
+                --save_prefix ${SP} \
+                --block_size 128 \
+                --overwrite_cache
+        done
+    done
+done
+
+echo "Done"
--- train_gptj.sh
+++ train_gptj.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH --gres=gpu:a100:4
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=492GB
-#SBATCH --time=8:00:00
-#SBATCH --job-name=train_bloom7b
-#SBATCH --output=train_bloom7b_%A_%a.out
+#SBATCH --gres=gpu:a100:3
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=360GB
+#SBATCH --time=48:00:00
+#SBATCH --job-name=train_gptj
+#SBATCH --output=train_gptj_%A_%a.out
 #SBATCH --array=0
 
 module purge
-module load cuda/11.6.2
+module load cuda/11.6.2    
 
 # which experiment
 EXPT="expt1"
@@ -19,15 +19,15 @@ EXS=("seen_data_0" "seen_data_1" "seen_data_2" "seen_data_3")
 LRS=(0.0001 0.00003 0.00001 0.000003)
 BSS=(1 2)
 
-# bloom-7b
-MO="bigscience/bloom-7b1"
+# gpt-j-6b
+MO="EleutherAI/gpt-j-6B"
 for EX in "${EXS[@]}"
 do
     for LR in "${LRS[@]}"
     do
         for BS in "${BSS[@]}"
         do
-            SP="bloom_7b_${EX}_${LR}_${BS}"
+            SP="gpt_j_${EX}_${LR}_${BS}"
             accelerate launch --config_file /scratch/eo41/lm-recognition-memory/accelerate_config.yaml --num_cpu_threads_per_process 4 /scratch/eo41/lm-recognition-memory/train.py \
                 --model_name_or_path ${MO} \
                 --train_file "/scratch/eo41/lm-recognition-memory/data/recognition-memory-experimental-data/${EXPT}/${EX}.json" \
