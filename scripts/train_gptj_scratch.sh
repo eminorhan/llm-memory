3bbea1fcb0131087620f2411303accc74bc76b4f
eminorhan <aeminorhan@gmail.com>
2023-03-30 16:24:43 -0400

    add scripts README

--- scripts/train_gptj_scratch.sh
+++ scripts/train_gptj_scratch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 #SBATCH --gres=gpu:a100:4
 #SBATCH --cpus-per-task=8
@@ -9,9 +9,6 @@
 #SBATCH --job-name=train_gptj_from_scratch
 #SBATCH --output=train_gptj_from_scratch_%A_%a.out
 #SBATCH --array=0
-
-module purge
-module load cuda/11.6.2    
 
 export TRANSFORMERS_CACHE="/vast/eo41/huggingface"
 export HF_DATASETS_CACHE="/vast/eo41/huggingface"
@@ -22,7 +19,7 @@ BS=4  # batch size
 MO="EleutherAI/gpt-j-6B"  # model architecture
 SP="gpt_j_scratch_wikitext103"  # save identifier
 
-accelerate launch --config_file /scratch/eo41/llm-memory/accelerate_config_big.yaml --num_cpu_threads_per_process 8 /scratch/eo41/llm-memory/train.py \
+accelerate launch --config_file /scratch/eo41/llm-memory/accelerate_config.yaml --num_cpu_threads_per_process 4 /scratch/eo41/llm-memory/train.py \
     --model_name_or_path ${MO} \
     --use_pretrained_weights False \
     --dataset_name wikitext \
